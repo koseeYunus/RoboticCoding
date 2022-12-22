ebd60606ff5f8d8a0b89ef7ea512c142d57d177d
Yunus KÖSE <kose.yunus.55@gmail.com>
2022-12-22 15:31:46 +0300

    update

---
+++ MesafeSensoru/mesafeSensoru/mesafeSensoru.ino
@@ -0,0 +1,32 @@
+#include "NewPing.h"
+
+#define Trig_Pin 9
+#define Echo_Pin 10
+#define Max_Uzaklik 400  
+
+NewPing sonar(Trig_Pin, Echo_Pin, Max_Uzaklik);
+
+float uzaklik;
+
+void setup() 
+{
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  uzaklik = sonar.ping_cm();
+  
+  Serial.print("Mesafe = ");
+  
+  if (uzaklik >= 400 || uzaklik <= 2) 
+  {
+    Serial.println("Mesafe algılanamadı");
+  }
+  else 
+  {
+    Serial.print(uzaklik);
+    Serial.println(" cm");
+  }
+  delay(500);
+}

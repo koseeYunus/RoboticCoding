b75e2a87b246af8af45567e03185d9c91d817587
Yunus KÖSE <kose.yunus.55@gmail.com>
2022-12-25 09:06:21 +0300

    25.12 added

---
+++ MesafeSensoru/mesafeSensoruDetay/mesafeSensoruDetay.ino
@@ -0,0 +1,52 @@
+int echoPin = 6
+int trigPin = 7
+#define buzzerPin 8
+
+int maxDeger = 400;
+int minDeger = 0;
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(buzzerPin, OUTPUT);
+}
+
+void loop() {
+
+  int olcum = mesafe(maxDeger, minDeger);
+  melodiCal(olcum*15);
+
+}
+
+int mesafe(int maxD, int minD)
+{
+  long gecenZaman, uzaklik;
+
+  digitalWrite(trigPin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  gecenZaman = pulseIn(echoPin, HIGH);
+  uzaklik = gecenZaman / 58.2;
+  delay(50);
+
+  if(uzaklik >= maxD || uzaklik <= minD)
+  {
+    return 0;
+  }
+  else{
+    return uzaklik;
+  }
+}
+
+int melodiCal(int beklemeSuresi)
+{
+  tone(buzzerPin, 256);
+  delay(beklemeSuresi);
+  
+  noTone(buzzerPin);
+  delay(beklemeSuresi);
+  
+}
